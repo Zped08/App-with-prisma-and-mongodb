8f6053fc5a70b3244378a0481c7cd3e8ca2610a3
Jair Flores <jairfloresa0802@gmail.com>
2023-05-26 12:37:43 +0200

    Creando componente panel de usarios donde se mostraran los usuarios

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,26 @@
-import { LoaderFunction } from '@remix-run/node'
-import { requireUserId } from '~/utils/auth.server'
+import { json, LoaderFunction } from "@remix-run/node";
+import { requireUserId } from "~/utils/auth.server";
+import { getOtherUsers } from "~/utils/user.server";
+import Layout from "~/components/Layout";
+import { UserPanel } from "~/components/user-panel";
+import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request)
-  return null
-}
+  //await requireUserId(request);
+  //return null; // <- Un cargador siempre tiene que devolver algún valor, incluso si es nulo
+  const userId = await requireUserId(request);
+  const users = await getOtherUsers(userId);
+  return json({ users });
+};
 
 export default function Home() {
-  return <h2>Home Page</h2>
+  const { users } = useLoaderData();
+  return (
+    <Layout>
+      <div className="h-full flex">
+        <UserPanel users={users} />
+        <div className="flex-1"></div>
+      </div>
+    </Layout>
+  );
 }
